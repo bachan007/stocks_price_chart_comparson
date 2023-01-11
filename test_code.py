1adf0efa141429da5d23a8a14aec98f6f5232162
bachan007 <bachannigam.nigam111@gmail.com>
2023-01-12 00:13:00 +0530

    created a demo price comparison chart

---
+++ test_code.py
@@ -0,0 +1,38 @@
+import yfinance as yf
+import plotly.graph_objects as go
+
+fig = go.Figure()
+
+def draw_lines(stock_list):
+    if len(stock_list)==1:
+        df = yf.download(stock_list[0])
+        fig.add_trace(go.Scatter(
+            x=df.index,
+            y=df['Adj Close'],
+            name=stock_list[0]
+        ))
+    if len(stock_list)>1:
+        for tckr in stock_list:
+            df = yf.download(tckr)
+            fig.add_trace(go.Scatter(
+            x=df.index,
+            y=df['Adj Close'],
+            name=tckr
+            ))
+
+    fig.update_layout(title=f"Price Movement Comparision between {stock_list}",
+    legend_title_text='Stocks',
+    xaxis_title='Date',
+    yaxis_title='Price in INR',
+    # plot_bgcolor='grey',
+    # paper_bgcolor='grey'
+    )
+
+    fig.update_xaxes(showgrid=False,)
+    fig.update_yaxes(showgrid=False)
+
+    fig.show()
+
+it_stock_list = ['TECHM.NS','WIPRO.NS','TCS.NS','INFY.NS','HCLTECH.NS','COFORGE.NS','LTI.NS']
+
+draw_lines(it_stock_list)
